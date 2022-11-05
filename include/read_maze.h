b63598ebb1d9b9bcc92ec7e6e33f346008599ae0
JayDJayG <j.d.guzman.v@gmail.com>
2022-11-05 18:07:57 -0500

    adding header files

---
+++ include/read_maze.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+enum Positions {ZERO, OBSTACLE, EMPTY_SPC, PATH, START, END};
+
+char* read_maze_parameters(char *path_to_file);
+
+int find_maze_height(char* maze_parameters);
+
+int find_maze_width(char* maze_parameters);
+
+char find_maze_symbols(char* maze_parameters, int symbol_of_interst);
